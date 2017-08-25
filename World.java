6fa17ffac3b97484e2cca238c568454e39eb4207
v98 <vrinda16279@iiitd.ac.in>
2017-08-25 08:38:21 +0530

    grassland

--- World.java
+++ World.java
@@ -2,7 +2,37 @@
 import java.io.*;
 import java.util.*;
 
+class grassland{
+	protected int availability;
+	protected final int[] coordinates;
+	protected final int radius;
 
+	public grassland(int units,int xcoord,int ycoord,int r){
+		this.availability=units;
+		this.coordinates=new int[2];
+		coordinates[0]=xcoord;
+		coordinates[1]=ycoord;
+		this.radius=r;
+	}
+
+	public int[] getCoords(){
+		return coordinates;
+	}
+
+	public int getRadius(){
+		return radius;
+	}
+
+	public int getA(){
+		return availability;
+	}
+
+	public void updateA(int ategrass){
+		availability-=ategrass;
+	}
+
+	
+}
 
 public class World{
 	public static void main(String[] args)throws IOException{
